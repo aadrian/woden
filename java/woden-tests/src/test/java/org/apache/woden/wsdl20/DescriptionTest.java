6e60b16e320f3e8e4a188a4d4df74863ac799815
sagara <sagara@13f79535-47bb-0310-9956-ffa450edef68>
2009-09-20 13:37:10 +0000

    fixed WODEN-152 - Woden shouldn't be dependent on Xerces
    git-svn-id: https://svn.apache.org/repos/asf/webservices/woden/trunk@817027 13f79535-47bb-0310-9956-ffa450edef68

--- java/woden-tests/src/test/java/org/apache/woden/wsdl20/DescriptionTest.java
+++ java/woden-tests/src/test/java/org/apache/woden/wsdl20/DescriptionTest.java
@@ -23,6 +23,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -42,9 +44,8 @@ import org.apache.woden.wsdl20.xml.ServiceElement;
 import org.apache.woden.wsdl20.xml.TypesElement;
 import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaCollection;
-import org.apache.xerces.parsers.DOMParser;
-import org.apache.xerces.xni.parser.XMLInputSource;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 /**
  * Unit tests for the Description component API.
@@ -93,15 +94,18 @@ public class DescriptionTest extends TestCase {
                     + "</complexType>" 
                     + "</schema>";
         
-        DOMParser builder = new DOMParser();
+        DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder builder = dbf.newDocumentBuilder();
         Reader reader = new StringReader(schemaString);
-        XMLInputSource is = new XMLInputSource(null, null, null, reader, null);
+        InputSource is = new InputSource(reader);
+        Document schemaDoc1 = null;
         try {
-            builder.parse(is);
+            schemaDoc1 = builder.parse(is);
         } catch(IOException e) {
             fail("There was an IOException whilst trying to parse the sample schema.");
         }
-        Document schemaDoc1 = builder.getDocument();
+        
         XmlSchemaCollection xsc = new XmlSchemaCollection();
         xs1 = xsc.read(schemaDoc1.getDocumentElement());
         schema.setSchemaDefinition(xs1);
